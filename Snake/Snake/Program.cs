a182db1dabe63d98e0a3f9cd3408903f837ea0fb
mrrrTom <dagoroff@gmail.com>
2018-06-05 11:25:33 +0300

    вернулись стенки

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -14,8 +14,8 @@ namespace Snake
             Console.SetWindowSize(80, 25);
             Console.SetBufferSize(80, 25);
             Snake snake = new Snake();
-           // Walls new_walls = new Walls(25, 80);
-            // new_walls.Draw();
+            Walls new_walls = new Walls(25, 80);
+             new_walls.Draw();
             while (true)
             {
                // snake.Move(Direction.Right);
